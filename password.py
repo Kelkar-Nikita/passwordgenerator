5aa4d930090456e9aa69170f4caa20f4fa0cbf15
Kelkar-Nikita <nkkelkar100@gmail.com>
2021-01-30 17:56:34 +0530

    initial commit.

---
+++ password.py
@@ -0,0 +1,12 @@
+# function to generate random passsword.
+# parameter: length of password
+# return: password string
+import random
+
+
+def passwordGenerator(length=5):
+    password = random.random()
+    password = str(int(password*(10**length)))
+    return password
+
+print(passwordGenerator(6))
